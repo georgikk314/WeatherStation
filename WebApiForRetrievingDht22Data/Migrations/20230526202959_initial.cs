1cae881da616a64c63e1554392eccd79987d63bd
georgikk314 <gogomen2005@gmail.com>
2023-05-26 23:50:53 +0300

    add web api

---
+++ WebApiForRetrievingDht22Data/Migrations/20230526202959_initial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApiForRetrievingDht22Data.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WeatherRecords",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Temperature = table.Column<double>(type: "float", nullable: false),
+                    Humidity = table.Column<double>(type: "float", nullable: false),
+                    Time = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WeatherRecords", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WeatherRecords");
+        }
+    }
+}
---
+++ WebApiForRetrievingDht22Data/Models/WeatherRecordsContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApiForRetrievingDht22Data.Models
+{
+    public class WeatherRecordsContext : DbContext
+    {
+        public WeatherRecordsContext(DbContextOptions<WeatherRecordsContext> options) : base(options)
+        { }
+
+        public DbSet<WeatherRecord> WeatherRecords { get; set; }
+    }
+}
