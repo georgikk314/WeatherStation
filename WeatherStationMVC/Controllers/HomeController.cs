632a44d3c367b610b79d1c0462b606b18811d4b4
Wsupp p <wsuppbruh@gmail.com>
2023-05-27 00:04:38 +0300

    add WeatherStationMVC website

---
+++ WeatherStationMVC/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using WeatherStationMVC.Models;
+
+namespace WeatherStationMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            
+            List<TemperatureAndHumidity> TempAndHumidity = new List<TemperatureAndHumidity>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("http://localhost:5097/api/WeatherRecords"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    TempAndHumidity = JsonConvert.DeserializeObject<List<TemperatureAndHumidity>>(apiResponse);
+                }
+            }        
+
+
+            return View(TempAndHumidity);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WeatherStationMVC/Models/TemperatureAndHumidity.cs
@@ -0,0 +1,10 @@
+﻿namespace WeatherStationMVC.Models
+{
+    public class TemperatureAndHumidity
+    {
+        public long Id { get; set; }
+        public double Temperature { get; set; }
+        public double Humidity { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
